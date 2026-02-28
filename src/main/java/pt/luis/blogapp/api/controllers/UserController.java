064729d465f9dc605461a48cdad1c494d0e856bc
Luís Santos <luissantos1996@live.com.pt>
2026-02-28 17:23:49 +0000

    feat(user): implement user controller and service.

--- src/main/java/pt/luis/blogapp/api/controllers/UserController.java
+++ src/main/java/pt/luis/blogapp/api/controllers/UserController.java
@@ -2,8 +2,19 @@ package pt.luis.blogapp.api.controllers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pt.luis.blogapp.api.entities.User;
+import pt.luis.blogapp.api.services.UserService;
 
-@Repository
-public interface UserController extends JpaRepository <User, Long> {
+@RestController
+@RequestMapping("/api/blog ")
+public class UserController{
+
+    private UserService userService;
+
+    public UserController(UserService userService){
+
+        this.userService = userService;
+    }
 }
--- src/main/java/pt/luis/blogapp/api/services/serviceImpl/UserServiceImpl.java
+++ src/main/java/pt/luis/blogapp/api/services/serviceImpl/UserServiceImpl.java
@@ -1,4 +1,16 @@
 package pt.luis.blogapp.api.services.serviceImpl;
 
-public class UserServiceImpl {
+import org.springframework.stereotype.Service;
+import pt.luis.blogapp.api.repositories.UserRepository;
+import pt.luis.blogapp.api.services.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository){
+
+        this.userRepository = userRepository;
+    }
 }
