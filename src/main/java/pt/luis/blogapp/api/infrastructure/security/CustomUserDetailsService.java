c23acf22cbfcae108cd25d646c160dca5142750e
Luís Santos <luissantos1996@live.com.pt>
2026-03-02 07:29:18 +0000

    feat(security): add JWT authentication and security configuration

---
+++ src/main/java/pt/luis/blogapp/api/infrastructure/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package pt.luis.blogapp.api.infrastructure.security;
+
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pt.luis.blogapp.api.entities.User;
+import pt.luis.blogapp.api.repositories.UserRepository;
+
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository
+                .findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username)
+                );
+        return new CustomUserDetails(user);
+    }
+}
+
--- src/main/java/pt/luis/blogapp/api/infrastructure/security/SecurityConfig.java
+++ src/main/java/pt/luis/blogapp/api/infrastructure/security/SecurityConfig.java
@@ -2,9 +2,15 @@ package pt.luis.blogapp.api.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -12,18 +18,54 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
+    private final JwtAuthFilter jwtAuthFilter;
+    private final CustomUserDetailsService customUserDetailsService;
+    private final PasswordHasher passwordHasher;
+
+    public SecurityConfig(
+            JwtAuthFilter jwtAuthFilter,
+            CustomUserDetailsService customUserDetailsService,
+            PasswordHasher passwordHasher
+    ){
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.customUserDetailsService = customUserDetailsService;
+        this.passwordHasher = passwordHasher;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 
         http
                 .csrf(crsf -> crsf.disable())
                 .headers(headers -> headers.frameOptions(f -> f.disable()))
+                .sessionManagement(session -> session.
+                        sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/h2-console/**")
                         .permitAll()
-                        .anyRequest()
+                        .requestMatchers("/auth/**")
                         .permitAll()
-                );
+                        .anyRequest()
+                        .authenticated()
+                )
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(customUserDetailsService);
+        provider.setPasswordEncoder(passwordHasher);
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration)
+            throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
