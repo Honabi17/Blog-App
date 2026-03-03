d3298e5a90bd557dea738e32fce33879bbb3c3f5
Luís Santos <luissantos1996@live.com.pt>
2026-03-03 19:28:32 +0000

    refactor: improve exception handling.

--- src/main/java/pt/luis/blogapp/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/pt/luis/blogapp/api/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package pt.luis.blogapp.api.exceptions;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +42,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ApiErrorResponse("An unexpected error occurred"));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiErrorResponse> handleBadRequest(BadRequestException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiErrorResponse(ex.getMessage()));
     }
 }
 
