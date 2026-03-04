5b3aa2193bb438da600a1b8f7ca68857bb6be075
Luís Santos <luissantos1996@live.com.pt>
2026-03-04 19:42:40 +0000

    feat(person): add PersonController with CRUD endpoints.

---
+++ src/main/java/pt/luis/blogapp/api/controllers/personControllers/PersonController.java
@@ -0,0 +1,54 @@
+package pt.luis.blogapp.api.controllers.personControllers;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pt.luis.blogapp.api.dto.personDTO.CreatePersonDTO;
+import pt.luis.blogapp.api.dto.personDTO.ProfileDTO;
+import pt.luis.blogapp.api.dto.personDTO.UpdatePersonDTO;
+import pt.luis.blogapp.api.services.personService.PersonService;
+
+@RestController
+@RequestMapping("/api/user")
+public class PersonController {
+
+    private PersonService personService;
+
+    public PersonController(PersonService personService){
+        this.personService = personService;
+    }
+
+    @PostMapping("/profile")
+    public ResponseEntity<ProfileDTO> createProfile(@RequestBody CreatePersonDTO dto){
+
+        ProfileDTO profile = personService.created(dto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(profile);
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<ProfileDTO> getProfile(){
+
+        ProfileDTO profile = personService.getProfile();
+
+        return ResponseEntity.ok(profile);
+    }
+
+    @PatchMapping("/profile")
+    public ResponseEntity<ProfileDTO> updateProfile(@RequestBody UpdatePersonDTO dto){
+
+        ProfileDTO update = personService.updated(dto);
+
+        return ResponseEntity.ok(update);
+    }
+
+    @DeleteMapping("/profile")
+    public ResponseEntity<Void> deleteProfile(){
+
+        personService.deleteProfile();
+
+        return ResponseEntity.noContent().build();
+    }
+
+}
