246409c6e3cdc6c4f5dea5aac0b464c1bb74f5ad
Luís Santos <luissantos1996@live.com.pt>
2026-03-08 09:26:19 +0000

    feat(post): add PostService interface with CRUD and pagination methods.
    - Added create(), update(), delete()
    - Added getByTitle(), getByAuthor()
    - Added getAllPaged() with sorting parameters

---
+++ src/main/java/pt/luis/blogapp/api/services/postService/PostService.java
@@ -0,0 +1,24 @@
+package pt.luis.blogapp.api.services.postService;
+
+
+import org.springframework.data.domain.Page;
+import pt.luis.blogapp.api.dto.postDTO.CreatePostDTO;
+import pt.luis.blogapp.api.dto.postDTO.PostResponseDTO;
+import pt.luis.blogapp.api.dto.postDTO.UpdatePostDTO;
+import java.util.List;
+
+
+public interface PostService {
+
+    PostResponseDTO create(CreatePostDTO dto);
+
+    PostResponseDTO getByTitle(String title);
+    List<PostResponseDTO> getByAuthor(String author);
+    Page<PostResponseDTO> getAllPaged(
+            int page, int size, String sortBy, String direction
+    );
+
+    PostResponseDTO update(Long id, UpdatePostDTO dto);
+
+    void delete(Long id);
+}
