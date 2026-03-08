f106986df74d996907ebcce1ea505560c5d78801
Luís Santos <luissantos1996@live.com.pt>
2026-03-08 09:28:27 +0000

    feat(post): add PostRepository with custom queries.
    - Added findByTitle()
    - Added findAllByAuthor_Username()
    - Added pageable findAll(Pageable pageable)

---
+++ src/main/java/pt/luis/blogapp/api/repositories/PostRepository.java
@@ -0,0 +1,21 @@
+package pt.luis.blogapp.api.repositories;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pt.luis.blogapp.api.models.entities.Post;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Optional<Post> findByTitle(String title);
+
+    List<Post> findAllByAuthor_Username(String username);
+
+    Page<Post> findAll(Pageable pageable);
+}
